6cf971c736f1f6a3d048489b7c6254fefd075686
gerry <yuanguohua_7818@163.com>
2019-06-15 23:18:26 +0800

    完善zk注册中心

--- src/main/java/com/tomato/framework/demo/client/ClientStarter.java
+++ src/main/java/com/tomato/framework/demo/client/ClientStarter.java
@@ -4,8 +4,6 @@ import com.tomato.framework.demo.facade.dto.PostDemo;
 import com.tomato.framework.demo.facade.service.HelloWorld;
 import com.tomato.framework.plugin.rmi.discovery.Discovery;
 import com.tomato.framework.plugin.rmi.discovery.ZKDiscovery;
-import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 /**
@@ -15,13 +13,15 @@ import java.rmi.RemoteException;
  */
 public class ClientStarter {
     
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+    public static void main(String[] args)
+        throws RemoteException, InterruptedException {
 //        Discovery discovery = new RmiDiscovery();
-        Discovery discovery = new ZKDiscovery("127.0.0.1", 2181);
+        Discovery discovery = new ZKDiscovery("127.0.0.1:2181", "rmi://127.0.0.1:18081");
         HelloWorld helloWorld = discovery.getBean(HelloWorld.class);
         System.out.println(helloWorld.hello("hello gerry!"));
         PostDemo postDemo = new PostDemo();
         postDemo.setName("gerry");
         System.out.println(helloWorld.post(postDemo));
+        Thread.sleep(1000 * 40);
     }
 }
